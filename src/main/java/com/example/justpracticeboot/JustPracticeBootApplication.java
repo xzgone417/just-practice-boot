76903b8024258b7d9234efcc64a0ee66c60798c4
xzg <xuzhenggang@egridcloud.com>
2023-06-07 17:36:51 +0800

    0001

---
+++ src/main/java/com/example/justpracticeboot/JustPracticeBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.justpracticeboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustPracticeBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustPracticeBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/justpracticeboot/JustPracticeBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.justpracticeboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustPracticeBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
