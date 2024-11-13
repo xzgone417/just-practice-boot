6920af3fd7e1429b2a85cbcfea0bcc1055976081
v_xzggxu <v_xzggxu@tencent.com>
2024-11-13 22:21:55 +0800

    大概i版本

--- assembly-line/src/test/java/xyz/xzgone/assemblyline/AssemblyLineApplicationTests.java
+++ assembly-line/src/test/java/xyz/xzgone/assemblyline/AssemblyLineApplicationTests.java
@@ -1,9 +1,8 @@
 package xyz.xzgone.assemblyline;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class AssemblyLineApplicationTests {
 
     @Test
