e648bc28259e46f573ab49040a8408b49591679d
v_xzggxu <v_xzggxu@tencent.com>
2024-11-14 10:07:13 +0800

    去掉test

--- assembly-line/src/test/java/xyz/xzgone/assemblyline/AssemblyLineApplicationTests.java
+++ assembly-line/src/test/java/xyz/xzgone/assemblyline/AssemblyLineApplicationTests.java
@@ -1,8 +1,9 @@
 package xyz.xzgone.assemblyline;
 
 import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-
+//@SpringBootTest
 class AssemblyLineApplicationTests {
 
     @Test
