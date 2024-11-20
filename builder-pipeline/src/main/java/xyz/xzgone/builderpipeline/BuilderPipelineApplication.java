109e6e93b2c1e859ab5d6bcae88d5f3aa9e9594c
v_xzggxu <v_xzggxu@tencent.com>
2024-11-20 10:57:08 +0800

    distence

---
+++ builder-pipeline/src/main/java/xyz/xzgone/builderpipeline/BuilderPipelineApplication.java
@@ -0,0 +1,13 @@
+package xyz.xzgone.builderpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilderPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuilderPipelineApplication.class, args);
+    }
+
+}
---
+++ builder-pipeline/src/test/java/xyz/xzgone/builderpipeline/BuilderPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.xzgone.builderpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilderPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
